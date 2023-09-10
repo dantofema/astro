5a70ba6e84f72dc3d77fde2d85d0a03fdb4d3185
Alejandro Leone <dantofema@gmail.com>
2023-09-10 14:13:21 -0300

    add ssr nodejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  output: 'server'
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
